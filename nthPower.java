64bb16ef285442fb9f83aceba17f90b31fd91180
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-08-08 13:27:37 +0800

    Remove logs. Modified the condition to less than or equal. Added exponent rules for exponents less than 2. Added error catching. Use BIG_TWO as base exponent.

--- nthPower.java
+++ nthPower.java
@@ -9,22 +9,30 @@ import static nthRoot.nthRoot.nthRoot;
 		And always run with <package-name>.<class-name> format
 */
 public class nthPower{
+    private static final BigDecimal BIG_TWO = new BigDecimal( "2" );
+
 	public static void main( String... parameterList ){
 		String value = parameterList[ 0 ];
 
 		String exponent = parameterList[ 1 ];
 
-		BigDecimal result = nthPower( value, exponent );
+        try{
+            BigDecimal result = nthPower( value, exponent );
 
-		System.out.print( result.toString( ) );
+            System.out.print( result.toString( ) );
+
+        }catch( Exception exception ){
+            System.err.print( exception.getMessage( ) );
+        }
 	}
 
-	public static final BigDecimal nthPower( String value, String exponent ){
+	public static final BigDecimal nthPower( String value, String exponent )
+        throws Exception
+    {
 		BigDecimal baseValue = new BigDecimal( value );
 		BigDecimal baseExponent = new BigDecimal( exponent );
 
 		String[ ] baseExponentPartList = baseExponent.toString( ).split( "\\." );
-        System.out.println( "baseExponentPartListLength: " + baseExponentPartList.length );
 		if( baseExponentPartList.length > 1 ){
 
 			String exponentDecimal = baseExponentPartList[ 1 ];
@@ -43,23 +51,34 @@ public class nthPower{
 			return phaseA.multiply( phaseC );
 
 		}else{
-            System.out.println( "baseExponent % 2 == 0: " + baseExponent.remainder( new BigDecimal( "2" ) ).compareTo( BigDecimal.ZERO )  );
-			if( baseExponent.remainder( new BigDecimal( "2" ) ).compareTo( BigDecimal.ZERO ) != 0 ){
+
+            if( baseExponent.compareTo( BigDecimal.ONE ) == 0 ){
+                return baseValue;
+            }
+
+            if( baseExponent.compareTo( BigDecimal.ZERO ) == 0 ){
+                return BigDecimal.ONE;
+            }
+
+            if( baseExponent.compareTo( BigDecimal.ZERO ) < 0 ){
+                throw new Exception( "cannot raise to negative number exponents" );
+            }
+
+            if( baseExponent.remainder( BIG_TWO ).compareTo( BigDecimal.ZERO ) != 0 ){
 				return nthPower( value, baseExponent.subtract( BigDecimal.ONE ).toString( ) ).multiply( baseValue );
 
 			}else{
 				BigDecimal result = baseValue;
-				BigDecimal partialBaseExponent = new BigDecimal( "2" );
 
-				BigDecimal procedureCount = baseExponent.divide( partialBaseExponent );
+				BigDecimal procedureCount = baseExponent.divide( BIG_TWO );
 				BigDecimal count = BigDecimal.ONE;
 
 				do{
-					result = result.pow( partialBaseExponent.intValue( ) );
+					result = result.pow( BIG_TWO.intValue( ) );
 
 					count = count.add( BigDecimal.ONE );
 
-				}while( count.compareTo( procedureCount ) != 0 );
+				}while( count.compareTo( procedureCount ) < 0 );
 
 				return result;    
 			}
